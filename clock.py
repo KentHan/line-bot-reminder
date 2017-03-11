c5c11e5c983c3b61857131d583bf817c9afcfc17
KentHan <kenthan.tw@gmail.com>
2017-03-11 22:20:06 +0800

    Add push_message

--- clock.py
+++ clock.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from pymongo import MongoClient
+
+from linebot import LineBotApi
+from linebot.models import TextMessage
+
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi(channel_access_token)
 
 mongodb_id = os.getenv('MONGODB_ID')
 mongodb_pw = os.getenv('MONGODB_PW')
@@ -13,6 +20,12 @@ client = MongoClient("mongodb://cluster1-shard-00-00-imdfs.mongodb.net:27017,clu
 client.admin.authenticate(mongodb_id, mongodb_pw, mechanism='SCRAM-SHA-1')
 db = client.user_data
 
+logging.basicConfig()
+
+def sendTextMessage(user_id, text):
+	message = TextMessage("1", text)
+	line_bot_api.push_message(user_id, message)
+
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=5)
@@ -20,5 +33,7 @@ def scheduled_job():
     print('This job is run every five seconds.')
     query = db["users"].find()
     print("DB count: %d" % query.count())
+    # sendTextMessage("U171903a51154a9693c8c49fbce6af0d1", "time is up!")
+    print('This job is finished.')
 
 sched.start()
