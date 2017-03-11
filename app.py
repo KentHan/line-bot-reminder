51f259335b39b4b3a1e6731d053c82a77817413e
KentHan <kenthan.tw@gmail.com>
2017-03-12 00:22:13 +0800

    Add conditional print

--- app.py
+++ app.py
@@ -92,7 +92,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("user_id: %s" % event.source.user_id)
+    if hasattr(event.source, "user_id"):
+        print("user_id: %s" % event.source.user_id)
     line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="I love you Lana~")
