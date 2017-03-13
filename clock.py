d2e5cc2d58082fc656fc8bee850183c590f0f702
KentHan <kenthan.tw@gmail.com>
2017-03-13 09:46:13 +0800

    Fix bug: duplicated notifications
    * Remove scheduled task

--- clock.py
+++ clock.py
@@ -10,8 +10,10 @@ from bo import EventBO
 
 bo = EventBO()
 
-sched = BlockingScheduler()
-@sched.scheduled_job('cron', minute="*")
-def scheduled_job():
-	bo.send_notification()
-sched.start()
+# sched = BlockingScheduler()
+# @sched.scheduled_job('cron', minute="*")
+# def scheduled_job():
+# 	bo.send_notification()
+# sched.start()
+
+bo.send_notification()
