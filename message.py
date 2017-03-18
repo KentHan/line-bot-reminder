e84cf5aa18b25d8dda98815322d96ab823401975
KentHan <kenthan.tw@gmail.com>
2017-03-18 23:40:43 +0800

    Fix bug

--- message.py
+++ message.py
@@ -13,7 +13,7 @@ class MessageApi():
 	def send_text_message(self, user_id, text):
 		send_text_message(user_id, text)
 
-def send_text_message(self, user_id, text):
+def send_text_message(user_id, text):
 	line_bot_api = LineBotApi(channel_access_token)
 	message = TextMessage("1", text)
 	line_bot_api.push_message(user_id, message)
