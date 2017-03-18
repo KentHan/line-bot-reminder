736931a7c806d9fbdba7a5e1c6d9b0242e9e92c9
KentHan <kenthan.tw@gmail.com>
2017-03-18 13:42:55 +0800

    Fix bug

--- event.py
+++ event.py
@@ -12,7 +12,7 @@ class Event:
 
 	def debug_print(self):
 		print("name: %s" % self.name, 
-			 	"created_time: %d" % self.created_time, 
-			  	"interval: %d" % self.interval,
-				"last_notified_time: %d" % self.last_notified_time)
-				"current_time: %d" % int(time()))
+			"created_time: %d" % self.created_time, 
+			"interval: %d" % self.interval,
+			"last_notified_time: %d" % self.last_notified_time,
+			"current_time: %d" % int(time()))
