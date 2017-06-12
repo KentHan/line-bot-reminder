8465c1587def04eeb9f776869a69ac12c2aa44e4
KentHan <kenthan.tw@gmail.com>
2017-06-12 23:20:15 +0800

    Add default last_notified_time for Event

--- event.py
+++ event.py
@@ -4,11 +4,12 @@ from time import time
 
 class Event:
 	
-	def __init__(self, target, name, created_time=0, interval=0):
+	def __init__(self, target, name, created_time=0, interval=0, last_notified_time=0):
 		self.target = target
 		self.name = name
 		self.created_time = created_time
 		self.interval = interval
+		self.last_notified_time = last_notified_time
 
 	def __str__(self):
 		return "target: {}\nname: {}\ncreated_time: {}\ninterval: {}\nlast_notified_time: {}\ncurrent_time: {}".format(
@@ -16,5 +17,5 @@ class Event:
 			self.name, 
 			self.created_time, 
 			self.interval, 
-			self.last_notified_time if hasattr(self, "last_notified_time") else 0, 
+			self.last_notified_time, 
 			int(time()))
